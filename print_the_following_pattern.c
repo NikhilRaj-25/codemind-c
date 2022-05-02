2f2043218f59ee0c8491c4863e192775c475003e
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-05-02 22:39:12 +0530

    #include<stdio.h> int main(){ 	int i,j,n; 	scanf("%d",&n); 	for(i=1;i<=n;i++){ 		for(j=1;j<=n;j++){ 			if(j==i){ 				printf("0"); 			} 			else{ 				printf("x"); 			} 		} 		printf(" "); 	} }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,23 +1,17 @@
 #include<stdio.h>
-int main()
-{
-    int i,j,n;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        int x=n-(i-1);
-        for(j=1;j<=n;j++)
-        {
-            if(j==i || j==x)
-            {
-                printf("x");
-            }
-            else
-            {
-                printf("0");
-            }
-        }
-        printf("
+int main(){
+	int i,j,n;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		for(j=1;j<=n;j++){
+			if(j==i){
+				printf("0");
+			}
+			else{
+				printf("x");
+			}
+		}
+		printf("
 ");
-    }
+	}
 }
