33b29763136935744d660b9d6f67920c22709900
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-05-02 23:25:43 +0530

    #include<stdio.h> int main() { 	int i,s,count=0; 	scanf("%d",&s); 	int arr[s]; 	for(i=0;i<s;i++) 	{ 		scanf("%d",&arr[i]); 	} 	for(i=0;i<s;i++) 	{ 		int x=arr[i]; 		if(x%2!=0) 		{ 			if(i%2==0) 			{ 				printf("False"); 				return 0; 			} 		} 	} 	printf("True"); }

---
+++ Strictly_ODD.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int i,s,count=0;
+	scanf("%d",&s);
+	int arr[s];
+	for(i=0;i<s;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<s;i++)
+	{
+		int x=arr[i];
+		if(x%2!=0)
+		{
+			if(i%2==0)
+			{
+				printf("False");
+				return 0;
+			}
+		}
+	}
+	printf("True");
+}
