bbda863bc402142957c2f548ee0b76d5c1a6c9d7
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-05-14 15:58:35 +0530

    #include<stdio.h> int main() {     int i,count=0;     char str[100];     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]==' ')         {             count++;         }     }     printf("%d",count); }

---
+++ count_spaces_in_a_string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,count=0;
+    char str[100];
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]==' ')
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
