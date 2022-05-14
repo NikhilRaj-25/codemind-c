a4d57cb83c2e19bf2d641de81f32cef7762bcf95
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-05-14 13:58:11 +0530

    #include<stdio.h> int main() {     int a,b,i;     scanf("%d",&a);     scanf("%d",&b);     int arr[a];     for(i=0;i<a;i++)     {         scanf("%d",&arr[i]);     }     int x,count=0,ans=0;     for(i=0;i<a;i++)     {         int r;         count=0;         x=arr[i];         if(x==0)         {             count++;         }         else         {             while(x)             {                 r=x%10;                 x=x/10;                 count++;             }         }         if(count==b)         {             ans++;         }     }     printf("%d",ans); }

---
+++ K_digit_length_numbers_in_a_array.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int arr[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int x,count=0,ans=0;
+    for(i=0;i<a;i++)
+    {
+        int r;
+        count=0;
+        x=arr[i];
+        if(x==0)
+        {
+            count++;
+        }
+        else
+        {
+            while(x)
+            {
+                r=x%10;
+                x=x/10;
+                count++;
+            }
+        }
+        if(count==b)
+        {
+            ans++;
+        }
+    }
+    printf("%d",ans);
+}
