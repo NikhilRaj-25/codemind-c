9bc4181a74d958127a98bc71fa7008c2f0cb05c0
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-07-08 18:40:34 +0530

    #include<stdio.h> int main() {     int a,b,c,i,j;     scanf("%d%d",&a,&b);     int arr[a][b];     for(i=0;i<a;i++)     {         for(j=0;j<b;j++)         {             scanf("%d",&arr[i][j]);         }     }     int s=0;     for(i=0;i<a;i++)     {         for(j=0;j<a;j++)         {             if(i==j || i+j==a-1)             {                 s+=arr[i][j];             }         }     }     printf("%d",s); }

--- sum_of_Diagonal_values.c
+++ sum_of_Diagonal_values.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,i,j;
+    int a,b,c,i,j;
     scanf("%d%d",&a,&b);
     int arr[a][b];
     for(i=0;i<a;i++)
@@ -11,27 +11,16 @@ int main()
             scanf("%d",&arr[i][j]);
         }
     }
-    int sum=0;
+    int s=0;
     for(i=0;i<a;i++)
     {
-        for(j=0;j<b;j++)
+        for(j=0;j<a;j++)
         {
-            if(i==j)
+            if(i==j || i+j==a-1)
             {
-                sum+=arr[i][j];
+                s+=arr[i][j];
             }
         }
     }
-    for(i=0,j=b-1;i<a,j>=0;i++,j--)
-    {
-        if(i==j)
-        {
-            continue;
-        }
-        else
-        {
-            sum+=arr[i][j];
-        }
-    }
-    printf("%d",sum);
+    printf("%d",s);
 }
