6db4e58c6bfdfa1defc4df64c45b78c7e8c56d1c
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-09-11 00:59:38 +0530

    #include<stdio.h> int main(){     int a,b,i,j,s=0;     scanf("%d%d",&a,&b);     int arr[a][b];     for(i=0;i<a;i++){         for(j=0;j<b;j++){             scanf("%d",&arr[i][j]);         }     }     for(i=0;i<a;i++){         s=0;         for(j=0;j<b;j++){             s=s+(arr[i][j]);         }         printf("%d ",s);     } }

--- Row_wise_sum_of_a_matrix.c
+++ Row_wise_sum_of_a_matrix.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main(){
-    int a,b,i,j,ans=0;
-    scanf("%d %d",&a,&b);
-    int arr[a][b],x=a-1;
+    int a,b,i,j,s=0;
+    scanf("%d%d",&a,&b);
+    int arr[a][b];
     for(i=0;i<a;i++){
         for(j=0;j<b;j++){
             scanf("%d",&arr[i][j]);
         }
     }
     for(i=0;i<a;i++){
-        int sum=0;
+        s=0;
         for(j=0;j<b;j++){
-            sum+=arr[i][j];
+            s=s+(arr[i][j]);
         }
-        printf("%d ",sum);
+        printf("%d ",s);
     }
 }
